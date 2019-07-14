8e359b9aaa2431ec61e2e677d9c4f1bc3865cbd0
Aditya Arora <10962267+arora-aditya@users.noreply.github.com>
2019-07-14 16:05:31 -0400

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/theialabs.ai/precache-manifest.df9281dd6cfef04aad00fdbb9079c775.js"
+  "/theialabs.ai/precache-manifest.07cf879d54139175a027d154a55ca70c.js"
 );
 
 self.addEventListener('message', (event) => {
